a7de81dfd05bbcbd727b1e83555c1c8bb327c704
JoAplon <158791447+JoAplon@users.noreply.github.com>
2024-03-17 18:15:45 -0800

    added layout and basic prompt

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# logo-maker
+# Logo Maker
+ 
+![Static Badge](https://img.shields.io/badge/MIT-license?style=flat-square&label=License&labelColor=%23cdcdcd&color=salmon)  
+
+## Description
+
+This program uses your input to generate a simple logo. It asks for a shape and color which will be put behind your choice of three characters. Then the logo will be made into an svg file that you can open in the browser.
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+You need to install package.json and jest to your terminal.
+
+## Usage
+
+This program can be used to make your own logo so you don't have to spend money on one. Especially if you just need a simple logo.
+
+## License
+
+This project is licensed under the MIT license.
+
+## Contributing
+
+N/A
+
+## Tests
+
+Pending..
+
+## Questions
+
+For questions or feedback regarding this project, please contact me at JoAplon, through my [github](https://github.com/JoAplon), or my email, jordan0aplon@gmail.com.
+
---
+++ index.js
@@ -0,0 +1,45 @@
+const inquirer = require('inquirer');
+const shapes = require('./lib/shapes.js');
+const fs = require('fs');
+
+inquirer
+    .prompt([
+        {
+             // Text for logo, has to be 3 characters
+             type: 'input',
+             name: 'text',
+             message: 'Enter the text you want on your logo (3 char total):',
+        },
+        {
+            // Color for text can be color name of hex value
+            type: 'input',
+            name: 'textColor',
+            message: 'Pick a color for the text (keyword or hex):',
+        },
+        {
+            // Pick a shape
+            type: 'checkbox',
+            name: 'shape',
+            message: 'Pick a shape for the logo:',
+            choices: [
+                { name: 'circle', value: 'circle' },
+                { name: 'triangle', value: 'triangle' },
+                { name: 'square', value: 'square' },
+            ]
+        },
+        {
+            // Pick the shape color can be color name or hex value
+            type: 'input',
+            name: 'shapeColor',
+            message: 'Pick a color for your shape:',
+        },
+    ])
+
+    // writes logo svg file and puts it into the examples folder. logs error if error, if no error logs message
+    fs.writeFile('svg.example', examples, (error) => {
+        if (error) {
+            console.log(error)
+        } else {
+            console.log('LOGO created!!')
+        }
+    });
