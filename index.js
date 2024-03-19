4cb7eabf9c0d00779ae6f8d56ff19daec556b5ad
JoAplon <158791447+JoAplon@users.noreply.github.com>
2024-03-19 10:16:40 -0700

    tests all pass and file being created in examples folder but logo not showing up how I customized it

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const inquirer = require('inquirer');
-const shapes = require('./lib/shapes.js');
+const Shapes = require('./lib/shapes.js');
 const fs = require('fs');
 
 inquirer
     .prompt([
         {
-             // Text for logo, has to be 3 characters
-             type: 'input',
-             name: 'text',
-             message: 'Enter the text you want on your logo (3 char total):',
-             validate: function(text) {
-             if(text.length > 3){
-                console.error('`text` can only be three characters!');
-                return false;
-                // if false then the validation failed
-             }
+            // Text for logo, has to be 3 characters
+            type: 'input',
+            name: 'text',
+            message: 'Enter the text you want on your logo (3 char total):',
+            validate: function (text) {
+                if (text.length > 3) {
+                    console.error('`text` can only be three characters!');
+                    return false;
+                    // if false then the validation failed
+                }
                 return true;
             }
         },
@@ -23,10 +23,10 @@ inquirer
             type: 'input',
             name: 'textColor',
             message: 'Pick a color for the text (keyword or hex):',
-            validate: function(textColor) {
+            validate: function (textColor) {
                 const colorNameRegex = /^(red|orange|yellow|green|blue|purple|white|black)$/i;
                 const hexValueRegex = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
-                if(colorNameRegex.test(textColor) || hexValueRegex.test(textColor)) {
+                if (colorNameRegex.test(textColor) || hexValueRegex.test(textColor)) {
                     return true;
                 } else {
                     console.error('Shucks! Must be a valid color name or hex value!')
@@ -50,10 +50,10 @@ inquirer
             type: 'input',
             name: 'shapeColor',
             message: 'Pick a color for your shape:',
-            validate: function(shapeColor) {
+            validate: function (shapeColor) {
                 const colorNameRegex = /^(red|orange|yellow|green|blue|purple|white|black)$/i;
                 const hexValueRegex = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
-                if(colorNameRegex.test(shapeColor) || hexValueRegex.test(shapeColor)) {
+                if (colorNameRegex.test(shapeColor) || hexValueRegex.test(shapeColor)) {
                     return true;
                 } else {
                     console.error('Shucks! Must be a valid color name or hex value!')
@@ -64,10 +64,42 @@ inquirer
     ])
 
     // writes logo svg file and puts it into the examples folder. logs error if error, if no error logs message
-    // fs.writeFile('svg.example', examples, (error) => {
-    //     if (error) {
-    //         console.log(error)
-    //     } else {
-    //         console.log('LOGO created!!')
-    //     }
-    // });
+    .then((response) => {
+
+        const folderPath = './examples';
+        const fileName = 'logo.svg';
+        const filePath = `${folderPath}/${fileName}`;
+        const circleSvg = `
+            <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="150" cy="100" r="80" fill="red" />
+                <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
+            </svg>
+            `;
+
+        const polygonSvg = `
+            <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+                <polygon points="100,50 160,180 40,180" fill="yellow" />
+                <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
+            </svg>
+            `;
+
+        const rectSvg = `
+            <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+                <rect width="100" height="100" x="10" y="10" rx="20" ry="20" fill="green" />
+                <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
+            </svg>
+            `;
+
+        const svgContent = circleSvg + polygonSvg + rectSvg;
+
+        fs.writeFile(filePath, svgContent, (error) => {
+            if (error) {
+                console.log(error)
+            } else {
+                console.log('LOGO created!!')
+            }
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
